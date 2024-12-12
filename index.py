1b33778678535c484f21f1317b72fa97f5e3ec15
sviatoslav-shubnikov <slavik20012008@gmail.com>
2024-12-12 13:20:03 +0300

    commit

--- index.py
+++ index.py
@@ -17,94 +17,107 @@ def get():
 def get_by_id():
 
 	numb = input("Введите id записи, по которой хотите сделать вывод информации: \n")
-	with open('city.json', 'r', encoding='utf-8') as file:
-				
-		data = json.load(file)
-				
-		found = False
 
-		for city in data:
+	if numb.isdigit():
 
-			if city['id'] == numb:
+		with open('city.json', 'r', encoding='utf-8') as file:
+					
+			data = json.load(file)
+					
+			found = False
 
-				print(f"\n=============== Найдено ===============") 
-				print(f"{city['id']} >> Название: {city['name']}, Страна: {city['country']}")
-				print(f"Является ли большим: {city['is_big']}, Численность населения: {city['people_count']}")
+			for city in data:
 
-				found = True
+				if city['id'] == numb:
 
-				break
+					print(f"\n=============== Найдено ===============") 
+					print(f"{city['id']} >> Название: {city['name']}, Страна: {city['country']}")
+					print(f"Является ли большим: {city['is_big']}, Численность населения: {city['people_count']}")
 
-		if not found:
+					found = True
 
-			print(f"\n=============== Не найдено ===============")
+					break
+
+			if not found:
+
+				print(f"\n=============== Не найдено ===============")
+
+	else:
+		print(f"ID не число.")
 
 
 def post():
 
-	
-	
-	with open('city.json', 'r+', encoding='utf-8') as file:
 
-		data = json.load(file)
+	new_name = input("Введите название города: \n")
+	new_country = input("Введите страны города: \n")
+	new_is_big = input("Введите является ли большим(>100k)(True/False): \n")=="True"
+	new_people_count = input("Введите численность населения города: \n")
+	
+	if new_people_count.isdigit():
+		with open('city.json', 'r+', encoding='utf-8') as file:
 
-		if not data:
-			new_id = 1
-		else:
+			data = json.load(file)
+
+			if not data:
+				new_id = 1
+			else:
+				
+				new_id = max(int(city['id']) for city in data) + 1
+
+			new_city={
+				"id": str(new_id),
+				"name": new_name,
+				"country": new_country,
+				"is_big": new_is_big,
+				"people_count": new_people_count
+			}
+
 			
-			new_id = max(int(city['id']) for city in data) + 1
+			data.append(new_city)
+			
+			
+			file.seek(0)
+			json.dump(data, file, ensure_ascii=False, indent=4)
+			file.truncate()
+
+		print("Запись добавлена!")
 	
-		new_name = input("Введите название города: \n")
-		new_country = input("Введите страны города: \n")
-		new_is_big = input("Введите является ли большим(>100k)(True/False): \n")=="True"
-		new_people_count = int(input("Введите численность населения города: \n"))
-
-		new_city={
-			"id": str(new_id),
-			"name": new_name,
-			"country": new_country,
-			"is_big": new_is_big,
-			"people_count": new_people_count
-		}
-
-		
-		data.append(new_city)
-		
-		
-		file.seek(0)
-		json.dump(data, file, ensure_ascii=False, indent=4)
-		file.truncate()
-
-	print("Запись добавлена!")
-
+	else:
+		print(f"Численность населения должно быть числом!")
 		
 
 
 def delete():
 
 	deletе = input("Введите номер записи которую вы хотите удалить: ")
+	
+	if deletе.isdigit():
+		with open('city.json', 'r+', encoding='utf-8') as file:
+			data = json.load(file)
+			found = False
 
-	with open('city.json', 'r+', encoding='utf-8') as file:
-		data = json.load(file)
-		found = False
+			for city in data:
+				if city['id'] == deletе:
+					data.remove(city)
+					found = True
+					break
 
-		for city in data:
-			if city['id'] == deletе:
-				data.remove(city)
-				found = True
-				break
+			if not found:
 
-		if not found:
+				print("\n=============== Не найдено ===============")
+				
+			else:
 
-			print("\n=============== Не найдено ===============")
-			
-		else:
+				file.seek(0)
+				json.dump(data, file, ensure_ascii=False, indent=4)
+				file.truncate()
 
-			file.seek(0)
-			json.dump(data, file, ensure_ascii=False, indent=4)
-			file.truncate()
+				print(f"Запись с {deletе} id удалена!")
 
-			print(f"Запись с {deletе} id удалена!")
+	else:
+		print("ID должно быть числом!")
+		
 
 
 def main():
@@ -122,19 +135,19 @@ def main():
 
 		res = input("\nВыберай (1/2/3/4/5): ")
 
-		if res =='1':
+		if res =="1":
 			
 			get()
 
 			count+=1
 
-		elif res == '2':
+		elif res == "2":
 
 			get_by_id()
 
 			count+=1
 
-		elif res == '3':
+		elif res == "3":
 
 			post()
 
